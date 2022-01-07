47653a490c1f83d2552d4ce1316f8962f2fba34f
kubby159 <83577397+kubby159@users.noreply.github.com>
2022-01-07 22:13:18 +0900

    [React] styled

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
-
+import "../scss/detail.scss";
+import Spinner from "../img/Spin-1s-200px.gif";
 function Detail() {
   const { id } = useParams();
   const [loading, setLoading] = useState(true);
@@ -15,25 +16,39 @@ function Detail() {
   useEffect(() => {
     getMovie();
   }, []);
-
+  console.log(movieDetail);
   return (
-    <div>
-      <h1>Detail</h1>
+    <div className="detail-container">
+      <h1 className="detail-title">Detail</h1>
+
       {loading ? (
-        <h2>loading...</h2>
+        <div className="loading">
+          <img src={Spinner} />
+        </div>
       ) : (
-        <div>
-          {movieDetail.map((movie) => {
-            return (
-              <div>
-                <h2>{movie.title}</h2>
-                {movie.genres.map((genre) => (
-                  <div>{genre}</div>
-                ))}
-                <p>{movie.description_intro}</p>
-              </div>
-            );
-          })}
+        <div className="detail-inner">
+          <div className="detail-movie">
+            {movieDetail.map((movie) => {
+              return (
+                <div className="detail-layout">
+                  <div className="detail-movie-img">
+                    <img src={movie.medium_cover_image} />
+                  </div>
+                  <div className="detail-movie-info">
+                    <h2 className="detail-movie-title">Title: {movie.title}</h2>
+                    {movie.genres.map((genre) => (
+                      <span>Genres: {genre}&nbsp;</span>
+                    ))}
+                    <p> RunTime: {movie.runtime}</p>
+                    <h2 className="detail-summary">Summary</h2>
+                    <p className="detail-description">
+                      {movie.description_intro}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </div>
       )}
     </div>
